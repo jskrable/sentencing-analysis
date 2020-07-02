2b1ef7a743fdf6434ef4e3cc7d74d1e54abb90ce
Jack Skrable <jskrable@bu.edu>
2020-07-02 10:25:59 -0400

    going thru important fields

--- read_file.py
+++ read_file.py
@@ -12,38 +12,53 @@ import pandas as pd
 
 sample = 1000
 
-with open('data/2019/opafy19nid.sas') as f:
-    sas = f.read()
 
-columns = sas.split('\n')[25:5937]
+def readDataFile(basedir):
+    with open(basedir + 'opafy19nid.sas') as f:
+        sas = f.read()
 
-issues = []
-cols = []
-for line in columns:
+    columns = sas.split('\n')[25:5937]
 
-    if len(line) < 75:
-        issues.append(line)
-    else:
-        col1 = [x for x in line[:25].split(' ') if x != '']
-        col2 = [x for x in line[25:50].split(' ') if x != '']
-        col3 = [x for x in line[50:].split(' ') if x != '']
-        cols.append(col1)
-        cols.append(col2)
-        cols.append(col3)
-
-
-def getColSpecs(col):
-    split = col[-1].split('-')
-    if len(split) > 1:
-        width = (int(split[0])-1, int(split[1]))
-    else:
-        width = (int(split[0])-1, int(split[0]))
-    
-    label = col[0]
-    return {'label':label, 'width':width}
+    issues = []
+    cols = []
+    for line in columns:
+        if len(line) < 75:
+            issues.append(line)
+        else:
+            col1 = [x for x in line[:25].split(' ') if x != '']
+            col2 = [x for x in line[25:50].split(' ') if x != '']
+            col3 = [x for x in line[50:].split(' ') if x != '']
+            cols.append(col1)
+            cols.append(col2)
+            cols.append(col3)
 
 
-specs = [getColSpecs(x) for x in cols[:-1]]
-names = [x['label'] for x in specs]
-width = [x['width'] for x in specs]
-df = pd.read_fwf('./data/2019/opafy19nid.dat', header=None, names=names, nrows=sample, colspecs=width)
+    def getColSpecs(col):
+        split = col[-1].split('-')
+        if len(split) > 1:
+            width = (int(split[0])-1, int(split[1]))
+        else:
+            width = (int(split[0])-1, int(split[0]))        
+        label = col[0]
+        return {'label':label, 'width':width}
+
+    specs = [getColSpecs(x) for x in cols[:-1]]
+    names = [x['label'] for x in specs]
+    width = [x['width'] for x in specs]
+    df = pd.read_fwf(
+        basedir + 'opafy19nid.dat', 
+        header=None, names=names, 
+        nrows=sample, colspecs=width)
+
+    return df
+
+
+# MAIN
+#####################
+
+df = readDataFile('./data/2019')
+
+
+keep = ['PRISDUM','NEWRACE','MONSEX','SENSPCAP','INTDUM','INOUT','PRISDUM','HRCOMSRV','FINE','DISPOSIT','DISTRICT','DEFCONSL','COMBDRG2','CAROFFAP','AMTFINEC','ALTMO','TIMESERV','TOTPRISN','TOTDAYS','TOTREST']
+
+df = df[keep]
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 This repo holds code to read and analyze federal court sentencing data from the [United States Sentencing Commision](https://www.ussc.gov/research/datafiles/commission-datafiles).
 
-Any of the individual offender datafiles from the link above can be downloaded and read into pandas using this code. The column headers are parsed manually out of the .sas file provided with the dataset on download.
+Any of the individual offender datafiles from the link above can be downloaded and read into pandas using this code. The column headers are parsed manually out of the .sas file provided with the dataset on download. Details on the columns in the dataset can be found [here](https://www.ussc.gov/sites/default/files/pdf/research-and-publications/datafiles/USSC_Public_Release_Codebook_FY99_FY19.pdf).
